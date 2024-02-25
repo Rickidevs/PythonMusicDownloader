f2388d4e7125ade3865bac4f0199256210b57d9b
Elvin Latifli <elvinlatiflinfo@gmail.com>
2024-02-25 12:46:34 +0300

    freezing problem solved

--- Main.py
+++ Main.py
@@ -3,10 +3,10 @@ import customtkinter
 import tempfile, base64, zlib
 from pytube import YouTube
 import requests
-import time
 from PIL import Image,ImageTk
 import os
 from pathlib import Path
+import threading
 
 
 # for remove tk iconbit
@@ -18,8 +18,6 @@ with open(ICON_PATH, 'wb') as icon_file:
     icon_file.write(ICON)
 
 
-
-
 # Set windows and configure
 screen = customtkinter.CTk()
 screen.geometry("400x500")
@@ -28,22 +26,25 @@ screen.title("Youtube Music Downloader")
 screen.iconbitmap(default=ICON_PATH)
 screen.resizable(False, False)
 
+
+#Fonts for text and button
 FONT_1=("Arial",25,"bold")
 FONT_2=("Arial",50,"bold")
 FONT_3=("Arial",30,"bold")
 FONT_4=("Arial",15)
 FONT_5=("Arial",16,"bold")
 FONT_6=("Times New Roman",20,"bold")
+FONT_7=("Arial",13)
 
+#global variable
 music_thub = ""
 music_title=""
-music_views=""
-music_rating=""
 yt = ""
+url = ""
 
 def toplevel_screen():
             
-            toplevel = customtkinter.CTkToplevel(screen)  # Pass the main window as the parent
+            toplevel = customtkinter.CTkToplevel(screen)
             toplevel.title("WARNING")
             toplevel.geometry("+%d+%d" % (screen.winfo_x()+90, screen.winfo_y()+240))
             toplevel.geometry("250x100")
@@ -52,28 +53,45 @@ def toplevel_screen():
             label.pack(padx=20, pady=20)
             toplevel.grab_set()
 
-def download_music():
-      
-      path_to_download = str(os.path.join(Path.home(), 'Downloads'))
-      stream=yt.streams.filter(only_audio=True).first()
-      downloaded_file = stream.download(path_to_download)
-      base, ext = os.path.splitext(downloaded_file)
-      new_file = base + '.mp3'
-      os.rename(downloaded_file, new_file)
-      copleted_label = customtkinter.CTkLabel(master=screen,text=f"Completed. You can find it in this: {path_to_download}",font=FONT_5,wraplength=300)
-      copleted_label.place(x=30,y=360)
 
+def do_download(url): 
+    try:
+        path_to_download = str(os.path.join(Path.home(), 'Downloads'))
+        stream = yt.streams.filter(only_audio=True).first()
+        downloaded_file = stream.download(path_to_download)
+        base, ext = os.path.splitext(downloaded_file)
+        new_file = base + '.mp3'
+        os.rename(downloaded_file, new_file)
+    
+        # İndirme tamamlandığında arayüz değişiklikleri
+        copleted_label = customtkinter.CTkLabel(master=screen,text=f"Completed. You can find it in this: {path_to_download}",font=FONT_5,wraplength=300)
+        copleted_label.place(x=30,y=360)
+    except Exception as e:
+        # İndirme hatası durumunda bir mesaj gösterme veya loglama mekanizması
+        print(f"Download error: {e}") 
+
+def start_download_thread():
+    thread = threading.Thread(target=do_download, args=(url,))
+    thread.start()
 
 def about_music():
+      global url
       url = music_thub
       image = Image.open(requests.get(url, stream=True).raw)
       new_size = (170, 140)  
       resized_image = image.resize(new_size) 
       tkimage = ImageTk.PhotoImage(resized_image)
-      music_thub_label = customtkinter.CTkLabel(master=screen,image=tkimage)
+      music_thub_label = customtkinter.CTkLabel(master=screen,image=tkimage,text="")
       music_thub_label.place(x=20,y=230)
-      music_title_label = customtkinter.CTkLabel(master=screen,text=music_title,font=FONT_6,wraplength=200)
-      music_title_label.place(x=168, y= 230)
+      music_title_label = customtkinter.CTkLabel(master=screen,text=music_title,font=FONT_6,)
+
+      if len(music_title) > 20:
+            music_edited_title = music_title[:20]
+            music_title_label.configure(text=f"{music_edited_title}....")
+            music_title_label.place(x=168, y= 230)
+      else:
+            music_title_label.place(x=168, y= 230)
+
       Download_button = customtkinter.CTkButton(master=screen,
                                  width=80,
                                  height=30,
@@ -85,28 +103,31 @@ def about_music():
                                  text_color="white",
                                  hover_color="light green",
                                  font=FONT_4,
-                                 command=download_music
+                                 command=start_download_thread
                                  )
       Download_button.place(x=180,y=310)
 
-def search_button():
-        music_link = link_entry.get()
-        if music_link == "":
-            toplevel_screen()
-        elif not music_link.startswith("https://youtu.be/"):
-            link_entry.delete(0, END)
-            toplevel_screen()
-        else:
-            link_entry.delete(0, END)
-            global yt
-            yt = YouTube(music_link)
-            global music_title,music_thub
-            music_title = yt.title
-            music_thub = yt.thumbnail_url
-            about_music()
+def do_search():
+    global url
+    music_link = link_entry.get()
+    if music_link == "":
+        toplevel_screen()
+    elif not music_link.startswith("https://youtu.be/"):
+        link_entry.delete(0, END)
+        toplevel_screen()
+    else:
+        link_entry.delete(0, END)
+        global yt
+        yt = YouTube(music_link)
+        global music_title, music_thub
+        music_thub = yt.thumbnail_url
+        music_title = yt.title
+        about_music()
 
+def search_thread():
+    thread = threading.Thread(target=do_search)
+    thread.start()
 
-                
 
 youtube_label = customtkinter.CTkLabel(master=screen,text="Youtube",font=FONT_1,text_color="green")
 youtube_label.place(x=50,y=30)
@@ -139,7 +160,7 @@ search_button = customtkinter.CTkButton(master=screen,
                                  text_color="white",
                                  hover_color="light green",
                                  font=FONT_4,
-                                 command=search_button
+                                 command=search_thread
                                  )
 
 search_button.place(x=290,y=180)
